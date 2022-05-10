0191a01fcb4284a6618ca1add8a4921751cd55a0
Isac Hansson <93720069+I-Hansson@users.noreply.github.com>
2022-05-10 21:13:28 +0200

    Add files via upload
    Inport

---
+++ iMat/iMat/src/iMat/iMat.java
@@ -0,0 +1,34 @@
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+public class iMat extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("first_main_page.fxml"));
+        primaryStage.setScene(new Scene(root, 1222, 880));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop(){
+        IMatDataHandler.getInstance().shutDown();
+    }
+
+
+
+
+}
+
+
---
+++ iMat/iMat/src/iMat/iMatController.java
@@ -0,0 +1,24 @@
+
+
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class iMatController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
