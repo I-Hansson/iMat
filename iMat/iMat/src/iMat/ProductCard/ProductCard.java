860506fb964e4d74742e5ea96a4e88ef34f421e3
Kasper Lundberg <kasperlundberg1@gmail.com>
2022-05-12 10:40:05 +0200

    flyttar iMatController till separat package för att göra det enkalre att komma åt. Dessutom ändringar i ProductCard

--- iMat/iMat/src/iMat/ProductCard/ProductCard.java
+++ iMat/iMat/src/iMat/ProductCard/ProductCard.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import Main.iMatController;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.awt.*;
 
@@ -29,8 +31,9 @@ private AnchorPane buyButton;
 @FXML
 private AnchorPane ecoAnchorPane;
 
+private iMatController iMatController;
 
-
+private IMatDataHandler handler;
 
 
 }
