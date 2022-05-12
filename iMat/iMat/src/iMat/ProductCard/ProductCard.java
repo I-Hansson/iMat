24472403779333dd44eb3cd36b5dc3cc3567be2f
Kasper Lundberg <kasperlundberg1@gmail.com>
2022-05-12 09:31:01 +0200

    gjort förändringar i ProductCard samt fxml filen. Lagt till informations png.

--- iMat/iMat/src/iMat/ProductCard/ProductCard.java
+++ iMat/iMat/src/iMat/ProductCard/ProductCard.java
@@ -1,8 +1,35 @@
 package ProductCard;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-public class ProductCard {
+import java.awt.*;
+
+public class ProductCard extends AnchorPane {
+
+@FXML
+private ImageView productImageView;
+@FXML
+private Label productNameLabel;
+@FXML
+private Label priceLabel;
+@FXML
+private TextField productAmount;
+@FXML
+private Button incButton;
+@FXML
+private Button decButton;
+@FXML
+private AnchorPane buyButton;
+@FXML
+private AnchorPane ecoAnchorPane;
+
+
 
 
 
